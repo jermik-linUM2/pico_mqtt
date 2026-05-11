01d3eea4676282d438efaf2ccb7988601ec7c8a3
jemi <jermikprotonmail.com>
2026-05-11 14:35:59 +0200

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pico_mqtt
 school project involving raspi pico and mqtt
+
+cd pico
+mkdir build
+cd build
+cmake -DWIFI_SSID="ssid" -DWIFI_PASSWORD="passphrase" ..
--- pico/main.c
+++ pico/main.c
@@ -6,6 +6,7 @@
 #include "bme280.h"
 #include "pico/cyw43_arch.h"
 #include "lwip/apps/mqtt_priv.h"
+#include <malloc.h>
 
 const char *MQTT_BROKER_URL = "192.168.1.100";
 
@@ -25,18 +26,29 @@ MQTT_CLIENT_DATA_T *mqtt;
 
 struct mqtt_connect_client_info_t mqtt_client_info = {
         "test",
-        "jemi",
+        "jemi", 
         NULL,      
-        15,        
-        NULL,      
-        NULL,      
+        15,      
+        NULL,     
+        NULL,     
         0,         
         0          
+#if LWIP_ALTCP && LWIP_ALTCP_TLS
         ,
         NULL
+#endif
 };
 
+//void print_memory_usage() {
+    //struct mallinfo info = mallinfo();
 
+    //printf("Total allocated: %d bytes\n", info.uordblks);
+    //printf("Total free: %d bytes\n", info.fordblks);
+    //printf("Total heap size: %d bytes\n", info.arena);
+    //printf("Largest free block: %d bytes\n", info.ordblks);
+//}
+
+/* Called when publish is complete either with sucess or failure */
 void mqtt_pub_request_cb(void *arg, err_t result)
 {
         if (result != ERR_OK){
@@ -48,6 +60,7 @@ static void mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len
 {
         printf("mqtt_incoming_publish_cb\n");
         MQTT_CLIENT_DATA_T *mqtt_client = (MQTT_CLIENT_DATA_T *)arg;
+        // strcpy(mqtt_client->topic, topic);
         strcpy((char *)mqtt_client->topic, (char *)topic);
 }
 
@@ -76,7 +89,7 @@ void mqtt_connect(MQTT_CLIENT_DATA_T *mqtt_client, void *arg)
         err = mqtt_client_connect(mqtt_client->mqtt_client_inst, &mqtt_ip, MQTT_PORT, mqtt_connection_cb, arg, &mqtt_client->mqtt_client_info);
         cyw43_arch_lwip_end();
         if (err != ERR_OK){
-                printf("mqtt_connect return %d\n", err);
+                //printf("mqtt_connect return %d\n", err);
         }
 }
 
@@ -102,7 +115,7 @@ int main() {
 	// Initialize mqtt client
         mqtt = (MQTT_CLIENT_DATA_T *)calloc(1, sizeof(MQTT_CLIENT_DATA_T));
         if (!mqtt){
-                printf("failed to allocate memory for mqtt client\n");
+                printf("Failed to allocate memory for mqtt client\n");
                 return -1;
         }
 
@@ -118,7 +131,7 @@ int main() {
                 return 0;
         }
 	
-	// Connect to mqtt broker
+	 //Connect to mqtt broker
         err_t err = mqtt_client_connect(mqtt->mqtt_client_inst,
                     &mqtt_ip, MQTT_PORT,
                     mqtt_connection_cb, LWIP_CONST_CAST(void *, &mqtt->mqtt_client_info),
@@ -133,11 +146,14 @@ int main() {
     	gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     	gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
 	gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+    	// Make the SPI pins available to picotool
     	bi_decl(bi_3pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI));
 
+    	// Chip select is active-low, so we'll initialise it to a driven-high state
     	gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
     	gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
     	gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
+    	// Make the CS pin available to picotool
     	bi_decl(bi_1pin_with_name(PICO_DEFAULT_SPI_CSN_PIN, "SPI CS"));
 
     	// See if SPI is working - interrograte the device for its I2C ID number, should be 0x60
@@ -155,8 +171,11 @@ int main() {
 	mqtt_connect(mqtt, mqtt);
 	free(mqtt);
     	while (1) {
-        	
+       		
+		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+		sleep_ms(500);
 		bme280_read_raw(&humidity, &pressure, &temperature);
+
 
         	temperature = compensate_temp(temperature);
         	pressure = compensate_pressure(pressure);
@@ -171,6 +190,8 @@ int main() {
 		sprintf(topic, "pico_bme280");
 		mqtt_publish(mqtt->mqtt_client_inst, "pico_bme280", buf, strlen(buf), 0, 0, mqtt_pub_request_cb, mqtt);
 		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+		//print_memory_usage();
+
         	sleep_ms(1000);
     	}
 }
